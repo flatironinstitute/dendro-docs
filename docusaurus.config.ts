1fc3adbcb40f26fb36080eb0616942fc4c2afd4c
luiz <luiz_tauffer@hotmail.com>
2024-05-18 12:50:25 +0200

    new social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,7 +58,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/dendro-social-card.jpg',
     navbar: {
       title: 'Dendro',
       logo: {
